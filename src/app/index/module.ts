a383f362cebbae3c1f5f05151188daa981304dc3
v.chastinet <victor.chastinet@victor.chastinet-pc>
2018-04-11 11:11:58 -0300

    [UPDATE] #0 - criação de um arquivo de index único

--- src/app/index/module.ts
+++
@@ -1,3 +0,0 @@
-export { LoadingGlobalModule } from '../loading-global/loading-global.module';
-export { LoadingModalModule } from '../loading-modal/loading-modal.module';
-export { LoadingInputModule } from '../loading-input/loading-input.module';
--- src/app/index/service.ts
+++
@@ -1,3 +0,0 @@
-export { LoadingGlobalService } from '../loading-global/loading-global.service';
-export { LoadingModalService } from '../loading-modal/loading-modal.service';
-export { LoadingInputService } from '../loading-input/loading-input.service';
