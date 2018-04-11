f3f5ff9479566302f4af366f14a149b9a21a4a43
v.chastinet <victor.chastinet@victor.chastinet-pc>
2018-04-11 11:26:58 -0300

    [ADD] #0 - arquivos compilados para github pages

--- src/app/lib/index.ts
+++
@@ -1,6 +0,0 @@
-export { LoadingGlobalModule } from './loading-global/loading-global.module';
-export { LoadingModalModule } from './loading-modal/loading-modal.module';
-export { LoadingInputModule } from './loading-input/loading-input.module';
-export { LoadingGlobalService } from './loading-global/loading-global.service';
-export { LoadingModalService } from './loading-modal/loading-modal.service';
-export { LoadingInputService } from './loading-input/loading-input.service';
